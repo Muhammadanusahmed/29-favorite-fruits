2e1f5821a9d3ca22b84b15bdbb895dd63abe8e09
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-23 18:10:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+// first we need to  do this make a tsconfig file and do some changes
+var favorite_fruits = ["mango", "apples", "bananas", "cherries"];
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mango!");
+}
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("cherries")) {
+    console.log("You really like cherries!");
+}
+if (favorite_fruits.includes("graps")) {
+    console.log("You really like graps!");
+}
+else {
+    console.log("you'r not like a graps");
+}
---
+++ app.ts
@@ -0,0 +1,21 @@
+// first we need to  do this make a tsconfig file and do some changes
+
+let favorite_fruits: string[] = ["mango", "apples", "bananas", "cherries"];
+
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mango!");
+} 
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("cherries")) {
+    console.log("You really like cherries!");
+} 
+if (favorite_fruits.includes("graps")) {
+    console.log("You really like graps!");
+}
+else{console.log("you'r not like a graps");
+} 
